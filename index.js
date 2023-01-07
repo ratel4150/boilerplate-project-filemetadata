8108548bcaa48044bb499f7241ab1c53ed255837
ratel4150 <rratel76@gmail.com>
2023-01-07 12:58:42 +0100

    commit first

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 var express = require('express');
+const multer=require('multer')
 var cors = require('cors');
+const bodyParser= require('body-parser')
 require('dotenv').config()
 
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(cors());
+
+const upload=multer({dest:"public/files"})
+
+
+
+
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post("/api/fileanalyse",upload.single("upfile"),(req,res)=>{
+ 
+  const{originalname,mimetype,size}=req.file
+  res.json({name:originalname,
+    type:mimetype,
+    size:size
+  })
+})
+
+
+
 
 
 
